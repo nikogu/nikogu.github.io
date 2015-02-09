666405cefc25b168b80a4b75887568fd9b373344
禺疆 <zhihao.gzh@alibaba-inc.com>
2015-02-09 15:55:10 +0800

    u

--- love/index.js
+++ love/index.js
@@ -204,9 +204,7 @@
     });
     loader.load();
 
-    $('#song')[0].addEventListener('canplaythrough', function() {
-        $('#song')[0].play();
-    }, false);
+    $('#song')[0].play();
 
 })();
 
