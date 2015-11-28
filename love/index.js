34d159a6215f2cb6dd12b89236b64076abcad39d
禺疆 <zhihao.gzh@alibaba-inc.com>
2015-11-28 11:52:36 +0800

    update

--- love/index.js
+++ love/index.js
@@ -1,5 +1,6 @@
 /**
- * jiayi.wu
+ * lw.l
+ *
  *
  * @author niko
  * @date 2015
@@ -37,103 +38,16 @@
 
     var flowExec = [];
 
-    //1
     flowExec.push(function() {
-        setTimeout(function() {
-            var slide = $('.swiper-slide').eq(0),
-                article = slide.find('.article'),
-                nextNode = slide.find('.next');
-
-            $('#J_time-1').typeTo('2015.4.12');
-
-            var duration = 2000;
-
-            var t = 2000,
-                spans = article.find('span');
-
-            spans.each(function(index, item) {
-                setTimeout(function() {
-                    $(item).css('opacity', 1);
-                    if ( index == spans.length-1 ) {
-                        nextNode.css('opacity', 1);
-                    }
-                }, t);
-                t += duration;
-            });
-        }, 500);
-    });
-
-    //2
-    flowExec.push(function() {
-
-        var slide = $('.swiper-slide').eq(1),
+        var slide = $('.swiper-slide').eq(0),
             article = slide.find('.article'),
             nextNode = slide.find('.next');
 
-        $('#J_time-2').typeTo('2015.4.13');
+        $('#J_time-1').typeTo('2015.11.28');
 
         var t = 2000,
             duration = 1000,
             spans = article.find('span');
-
-        setTimeout(function() {
-            setStep(14, 25, $('#J_time-2'));
-        }, t+100);
-        spans.each(function(index, item) {
-            setTimeout(function() {
-                $(item).css('opacity', 1);
-                if ( index == spans.length-1 ) {
-//                    nextNode.css('opacity', 1);
-                    $('#J_tips-2').typeTo('不知道从什么时候，听你说自己的事情，是让我最开心事情 ^_^');
-                    setTimeout(function() {
-                        nextNode.css('opacity', 1);
-                    }, 2000);
-                }
-            }, t);
-            t += duration;
-        });
-    });
-
-    //3
-    flowExec.push(function() {
-
-        var slide = $('.swiper-slide').eq(2),
-            article = slide.find('.article'),
-            nextNode = slide.find('.next');
-
-        $('#J_time-3').typeTo('2015.5.10');
-
-        var t = 2000,
-            duration = 1000,
-            spans = article.find('span');
-
-        spans.each(function(index, item) {
-            setTimeout(function() {
-                $(item).css('opacity', 1);
-                if ( index == spans.length-1 ) {
-                    nextNode.css('opacity', 1);
-                }
-            }, t);
-            t += duration;
-        });
-
-    });
-
-    //4
-    flowExec.push(function() {
-        var slide = $('.swiper-slide').eq(3),
-            article = slide.find('.article'),
-            nextNode = slide.find('.next');
-
-        $('#J_time-4').typeTo('2015');
-
-        var t = 2000,
-            duration = 1000,
-            spans = article.find('span');
-
-        setTimeout(function() {
-            setStep(2015, 9999, $('#J_time-4'), 100, 4);
-        }, t+100);
 
         spans.each(function(index, item) {
             setTimeout(function() {
@@ -141,7 +55,7 @@
                 if ( index == spans.length-1 ) {
                     window._love();
                     setTimeout(function() {
-                        $('#logo').typeTo('love chen');
+                        $('#logo').typeTo('To L.WL');
                     }, 4500);
                 }
             }, t);
