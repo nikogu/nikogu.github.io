bf04a68de5509c41101345f608269a9b1c849db9
nikogu <644506165@qq.com>
2018-09-22 14:54:13 +0800

    add button

--- car/back.js
+++ car/back.js
@@ -1,23 +1,42 @@
-(function($scope, $timeout) {
-
+var Car = function(config) {
+  // ref: http://data.acureus.com/sim/
+  this.name = config.name;
+  this.$scope = {};
+  var $scope = this.$scope;
   var $timeout = setTimeout;
   var $scope = {};
-  $scope.weight = 1500;
   $scope.drag = 0.3;
   $scope.A = 2.0;
-
-  $scope.tw = 205;
-  $scope.tr = 55;
-  $scope.rh = 16;
-
-  $scope.shiftDelay = 600;
   $scope.rag = 3.46;
-  $scope.gears = new Array("4.5", "2.5", "1.7", "1.2", "1", "0.85", "0.7");
-  $scope.gearCount = 6;
   $scope.switchPoint = 100;
-  $scope.maxRPM = 6400;
-  $scope.torque = 200;
 
+  // edit
+  $scope.weight = config.weight || 1500;
+
+  // tire
+  $scope.tw = config.tire.tw || 205;
+  $scope.tr = config.tire.tr || 55;
+  $scope.rh = config.tire.rh || 16;
+
+  // gear
+  $scope.shiftDelay = config.shiftDelay || 600;
+  $scope.gears = config.gears || [
+    "4.5",
+    "2.5",
+    "1.7",
+    "1.2",
+    "1",
+    "0.85",
+    "0.7"
+  ];
+  $scope.gearCount = config.gears || 6;
+
+  // engine
+  $scope.maxRPM = config.maxRPM || 6400;
+  $scope.torque = config.torque || 200;
+
+  // engine type
+  // 0: natura 1: turbo
   $scope.torqueCharacteristics = 0;
 
   $scope.updateGears = function() {
@@ -33,6 +52,7 @@
     }
   };
 
+  $scope.maxSpeed = 200;
   $scope.currentGear = 1;
   $scope.currentSpeed = 0;
   $scope.currentRPM = 0;
@@ -57,6 +77,7 @@
     iteration,
     idx,
     switchP;
+
   $scope.run = function() {
     $scope.buttonEnabled = false;
     $scope.data = [
@@ -165,24 +186,45 @@
           $scope.gears[$scope.currentGear - 1];
         var Fa = Fm - Fw;
         var a = Fa / $scope.weight;
+
         $scope.currentAcc = parseFloat(a).toFixed(2);
+        // console.log('currentAcc', $scope.currentAcc);
+
         speed = a * stepSize + speed;
         rotation = speed / uRad;
         rotation =
           rotation * $scope.rag * $scope.gears[$scope.currentGear - 1] * 60;
+
         $scope.currentRPM = parseFloat(rotation).toFixed(2);
+        // console.log('currentRPM', $scope.currentRPM);
+
         $scope.currentSpeed = parseFloat(speed * 3.6).toFixed(2);
+        // console.log('currentSpeed', $scope.currentSpeed);
+
         if ($scope.speedLabel50 == undefined && $scope.currentSpeed >= 50) {
           $scope.speedLabel50 =
             "50 km/h reached: " + parseFloat(i).toFixed(1) + "s";
+          console.log(
+            "** " + config.name + " 50 km/h",
+            parseFloat(i).toFixed(1)
+          );
         }
         if ($scope.speedLabel100 == undefined && $scope.currentSpeed >= 100) {
           $scope.speedLabel100 =
             "100 km/h reached: " + parseFloat(i).toFixed(1) + "s";
+          config.node.querySelector('em').innerHTML += ' / ' + parseFloat(i).toFixed(1) +'s / ';  
+          console.log(
+            "** " + config.name + " 100 km/h",
+            parseFloat(i).toFixed(1)
+          );
         }
         if ($scope.speedLabel200 == undefined && $scope.currentSpeed >= 200) {
           $scope.speedLabel200 =
             "200 km/h reached: " + parseFloat(i).toFixed(1) + "s";
+          console.log(
+            "** " + config.name + " 200 km/h",
+            parseFloat(i).toFixed(1)
+          );
         }
         if (
           $scope.currentSpeed + 25 >
@@ -505,4 +547,241 @@
   ];
   $scope.torqueSeries = ["Torque Characteristics"];
   $scope.torqueData = [[0, 45, 56, 67, 78, 89, 100, 100, 85, 65, 45]];
+
+  // exec
+  this.run = $scope.run;
+  this.currentSpeed = function() {
+    return $scope.currentSpeed;
+  };
+  this.move = function(x) {
+    config.node.style.transform = "translate(" + x + "px, 0px)";
+  };
+  this.x = 0;
+  this.node = config.node;
+};
+
+(function() {
+  var BMW_M2 = new Car({
+    name: "BMW_M2",
+    node: document.getElementById("bmw_m2"),
+    weight: 1595,
+    tire: {
+      tw: 265,
+      tr: 35,
+      rh: 19
+    },
+    shiftDelay: 200,
+    gearCount: 7,
+    maxRPM: 6400,
+    torque: 465
+  });
+
+  var FOCUS_ST = new Car({
+    name: "FOCUS_ST",
+    node: document.getElementById("focus_st"),
+    weight: 1452,
+    tire: {
+      tw: 235,
+      tr: 40,
+      rh: 18
+    },
+    shiftDelay: 500,
+    gearCount: 6,
+    maxRPM: 5500,
+    torque: 345
+  });
+
+  var FOCUS_RS = new Car({
+    name: "FOCUS_RS",
+    node: document.getElementById("focus_rs"),
+    weight: 1572,
+    tire: {
+      tw: 235,
+      tr: 35,
+      rh: 19
+    },
+    shiftDelay: 500,
+    gearCount: 6,
+    maxRPM: 6000,
+    torque: 440
+  });
+
+  var AUDI_RS3 = new Car({
+    name: "AUDI_RS3",
+    node: document.getElementById("audi_rs3"),
+    weight: 1520,
+    tire: {
+      tw: 235,
+      tr: 35,
+      rh: 19
+    },
+    shiftDelay: 200,
+    gearCount: 7,
+    maxRPM: 7000,
+    torque: 480
+  });
+
+  var BENZ_A45 = new Car({
+    name: "BENZ_A45",
+    node: document.getElementById("benz_a45"),
+    weight: 1585,
+    tire: {
+      tw: 235,
+      tr: 40,
+      rh: 18
+    },
+    shiftDelay: 200,
+    gearCount: 7,
+    maxRPM: 6000,
+    torque: 450
+  });
+
+  var Ferrari_458 = new Car({
+    name: "Ferrari_458",
+    node: document.getElementById("ferrari_458"),
+    weight: 1395,
+    tire: {
+      tw: 305,
+      tr: 30,
+      rh: 20
+    },
+    shiftDelay: 100,
+    gearCount: 4,
+    maxRPM: 9000,
+    torque: 540
+  });
+
+  var mclaren_p1 = new Car({
+    name: "McLaren P1",
+    node: document.getElementById("mclaren_p1"),
+    weight: 1395,
+    tire: {
+      tw: 315,
+      tr: 30,
+      rh: 20
+    },
+    shiftDelay: 100,
+    gearCount: 7,
+    maxRPM: 7500,
+    torque: 720
+  });
+
+  // cars
+  var cars = [BMW_M2, FOCUS_ST, FOCUS_RS, AUDI_RS3, BENZ_A45, Ferrari_458, mclaren_p1];
+
+  // 1km = 1000px
+  // 1m = 1px
+  // 400m
+  var ot = new Date().getTime();
+  var dt = 0;
+  var nt;
+  var radio = 2;
+  var count = 0;
+  var distance = 400 * radio;
+  var math;
+  var beginTime;
+  var btn = document.getElementById("btn");
+  var add = document.getElementById("add");
+
+  function go() {
+    nt = new Date().getTime();
+    dt = nt - ot;
+    ot = nt;
+
+    math = (((1000 / 60 / 60) * dt) / 1000) * radio;
+
+    cars.forEach(function(car) {
+      car.x += car.currentSpeed() * math;
+      car.move(car.x);
+
+      if (car.x >= distance) {
+        console.log(car.name, (new Date().getTime() - beginTime) / 1000);
+
+        car.node.querySelector('em').innerHTML +=
+          " " + (new Date().getTime() - beginTime) / 1000 + "s" + " / " + car.currentSpeed() + 'km/h';
+
+        cars.forEach(function(c, index) {
+          if (c.name === car.name) {
+            cars.splice(index, 1);
+          }
+        });
+        count++;
+      }
+    });
+
+    if (cars.length > 0) {
+      requestAnimationFrame(go);
+    } else {
+      btn.style.display = "inline";
+      add.style.display = "inline";
+    }
+  }
+
+  btn.addEventListener("click", function(e) {
+    var target = e.target;
+    if (target.innerHTML === "Reset") {
+      location.reload();
+      return;
+    }
+    target.style.display = "none";
+    add.style.display = "none";
+    target.innerHTML = "Reset";
+    cars.forEach(function(car) {
+      car.run();
+    });
+    beginTime = new Date().getTime();
+    requestAnimationFrame(go);
+  });
+
+  var gearMap = [200, 500, 500, 200, 200, 100];
+
+  var form = document.getElementById('form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    try {
+      var formData = new FormData(form);
+      var name = formData.get('name');
+      var weight = formData.get('weight');
+      var power = formData.get('power');
+      var maxRPM = formData.get('maxrpm');
+      var torque = formData.get('torque');
+      var tire = formData.get('tire');
+      var gears = formData.get('gears');
+      var geartype = formData.get('geartype');
+
+      const ts = tire.split('/');
+
+      const id = 'node' + new Date().getTime();
+
+      var newNode = document.createElement('div');
+      newNode.id = id;
+      newNode.className = 'car';
+      newNode.style.background = 'black';
+      newNode.style.top = (cars.length * 50 + 50) + 'px';
+      newNode.innerHTML = '<span>' + name + '</span><em></em>';
+
+      document.getElementById('container').appendChild(newNode);
+
+      const newCar = new Car({
+        name: name,
+        node: document.getElementById(id),
+        weight: weight,
+        tire: {
+          tw: ts[0],
+          tr: ts[1],
+          rh: ts[2],
+        },
+        shiftDelay: gearMap[geartype*1],
+        gearCount: gears,
+        maxRPM: maxRPM,
+        torque: torque
+      });
+
+      cars.push(newCar);
+
+    } catch(e) {
+      throw new Error(e);
+      alert('不要瞎几把乱搞');
+    }
+  });
 })();
