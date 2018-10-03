eea673a18e6615bfc4810cead2c770e7f9abc0bd
nikogu <644506165@qq.com>
2018-10-03 17:41:36 +0800

    完善 cell

--- cell/index.js
+++ cell/index.js
@@ -1,127 +1,243 @@
 $(function() {
-  // cell
-  var cell = $("#cell")[0];
+  function random(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
+  }
 
-  var cellAnim = Amo.keyframes({
-    0: {
-      transform: "translate(15px, 15px)"
-    },
-    20: {
-      transform: "translate(-3px, -2px)"
-    },
-    40: {
-      transform: "translate(-13px, 15px)"
-    },
-    60: {
-      transform: "translate(-4px, 2px)"
-    },
-    70: {
-      transform: "translate(12px, 12px)"
-    },
-    80: {
-      transform: "translate(-11px, -11px)"
-    },
-    100: {
-      transform: "translate(17px, 13px)"
+  function randomTranslate(a, b) {
+    return (
+      "translate(" + random(a[0], a[1]) + "px, " + random(b[0], b[1]) + "px)"
+    );
+  }
+
+  function randomRotate(a, b) {
+    return "rotate(0" + random(a, b) + "deg)";
+  }
+
+  var animationMaker = function() {
+    var cellAnim = Amo.keyframes({
+      0: {
+        transform: randomTranslate([-15, 15], [-15, 15])
+      },
+      20: {
+        transform: randomTranslate([-10, 10], [-10, 10])
+      },
+      40: {
+        transform: randomTranslate([-5, 5], [-5, 5])
+      },
+      60: {
+        transform: randomTranslate([-15, 15], [-15, -15])
+      },
+      70: {
+        transform: randomTranslate([-10, 10], [-10, 10])
+      },
+      80: {
+        transform: randomTranslate([-5, 5], [-5, 5])
+      },
+      100: {
+        transform: randomTranslate([-15, 15], [-15, 15])
+      }
+    }).animate({
+      mode: "forwards",
+      duration: random(5000, 15000),
+      easing: "linear",
+      time: "infinite",
+      direction: "alternate"
+    });
+
+    var cellBodyAnim = Amo.keyframes({
+      0: {
+        transform: randomRotate(-40, 40)
+      },
+      20: {
+        transform: randomRotate(-10, 10)
+      },
+      40: {
+        transform: randomRotate(-60, 60)
+      },
+      60: {
+        transform: randomRotate(-80, 80)
+      },
+      80: {
+        transform: randomRotate(-140, 140)
+      },
+      100: {
+        transform: randomRotate(-100, 100)
+      }
+    }).animate({
+      mode: "forwards",
+      duration: random(2000, 4000),
+      easing: "linear",
+      time: "infinite",
+      direction: "alternate"
+    });
+
+    var c1Anim = Amo.keyframes({
+      0: {
+        transform: randomRotate(-20, 20)
+      },
+      20: {
+        transform: randomRotate(-20, 20)
+      },
+      40: {
+        transform: randomRotate(-20, 20)
+      },
+      60: {
+        transform: randomRotate(-20, 20)
+      },
+      80: {
+        transform: randomRotate(-20, 20)
+      },
+      100: {
+        transform: randomRotate(-20, 20)
+      }
+    }).animate({
+      mode: "forwards",
+      duration: random(2000, 3000),
+      easing: "linear",
+      time: "infinite",
+      direction: "alternate"
+    });
+
+    var c2Anim = Amo.keyframes({
+      0: {
+        transform: randomRotate(-20, 20)
+      },
+      20: {
+        transform: randomRotate(-20, 20)
+      },
+      40: {
+        transform: randomRotate(-20, 20)
+      },
+      60: {
+        transform: randomRotate(-20, 20)
+      },
+      80: {
+        transform: randomRotate(-20, 20)
+      },
+      100: {
+        transform: randomRotate(-20, 20)
+      }
+    }).animate({
+      mode: "forwards",
+      duration: random(2000, 3000),
+      easing: "linear",
+      time: "infinite",
+      direction: "alternate"
+    });
+
+    return {
+      cellAnim: cellAnim,
+      cellBodyAnim: cellBodyAnim,
+      c1Anim: c1Anim,
+      c2Anim: c2Anim
+    };
+  };
+
+  var Cell = function(config, index) {
+    // cell
+    var id = "cell" + index;
+    var node =
+      '<div class="cell-wrapper">' +
+      '<div id="' +
+      id +
+      '" class="cell">' +
+      '<img class="cell-img" src="' +
+      config.image +
+      '"/>' +
+      '<div class="bg1 cell-bg" style="background-color: ' +
+      config.color +
+      ';"></div>' +
+      '<div class="bg2 cell-bg" style="background-color: ' +
+      config.color +
+      ';"></div>' +
+      '<div class="name">' +
+      config.name +
+      '<img class="cell-icon" src="' +
+      config.icon +
+      '"/>' +
+      "</div>" +
+      '<div class="jpname">' +
+      config.jpname +
+      "</div>" +
+      "</div>" +
+      "</div>";
+
+    var run = function() {
+      // cell body
+      var cellRoot = $("#" + id);
+      var cellBody = cellRoot.find("img")[0];
+      var c1 = cellRoot.find(".bg1")[0];
+      var c2 = cellRoot.find(".bg2")[0];
+
+      var anim = animationMaker();
+      anim.cellAnim.run(cellRoot);
+      anim.cellBodyAnim.run(cellBody);
+      anim.c1Anim.run(c1);
+      anim.c2Anim.run(c2);
+    };
+
+    return {
+      node: node,
+      run: run
+    };
+  };
+
+  function makeCell(data) {
+    var cells = [];
+    var root = $("#root");
+    var fragment = $("<div/>");
+    for (var i = 0; i < data.length; i++) {
+      var cell = new Cell(data[i], i);
+      fragment.append(cell.node);
+      cells.push(cell);
     }
-  }).animate({
-    mode: "forwards",
-    duration: 10000,
-    easing: "linear",
-    time: "infinite",
-    direction: "alternate"
-  });
+    root.append(fragment);
+    return cells;
+  }
 
-  cellAnim.run(cell);
+  var cellData = window.cellData || [];
+  var loaded = false;
+  var loading = $("#loading");
 
-  // cell body
-  var cellBody = $("#cell-body")[0];
-
-  var cellBodyAnim = Amo.keyframes({
-    0: {
-      transform: "rotate(0deg)"
-    },
-    20: {
-      transform: "rotate(30deg)"
-    },
-    40: {
-      transform: "rotate(10deg)"
-    },
-    60: {
-      transform: "rotate(90deg)"
-    },
-    70: {
-      transform: "rotate(80deg)"
-    },
-    80: {
-      transform: "rotate(120deg)"
-    },
-    100: {
-      transform: "rotate(100deg)"
+  function onLoaded() {
+    if (loaded) return;
+    $("#root").css("opacity", 1);
+    loading.hide();
+    loaded = true;
+    var cells = makeCell(cellData);
+    for (var i = 0; i < cells.length; i++) {
+      cells[i].run();
     }
-  }).animate({
-    mode: "forwards",
-    duration: 4000,
-    easing: "linear",
-    time: "infinite",
-    direction: "alternate"
-  });
+  }
 
-  cellBodyAnim.run(cellBody);
+  // preload
+  (function() {
+    var node = $("#preload");
+    var count = 0;
+    var onloadFunc = function() {
+      count++;
+      if (count >= cellData.length * 2) {
+        onLoaded();
+      }
+    };
+    var fragment = $("<div />");
+    for (var i = 0; i < cellData.length; i++) {
+      var image1 = new Image();
+      image1.src = cellData[i].image;
+      image1.onload = onloadFunc;
 
-  // c1
-  var c1 = $("#circle1")[0];
-  var c1Anim = Amo.keyframes({
-    0: {
-      transform: "rotate(-10deg)"
-    },
-    20: {
-      transform: "rotate(20deg)"
-    },
-    60: {
-      transform: "rotate(-5deg)"
-    },
-    80: {
-      transform: "rotate(13deg)"
-    },
-    100: {
-      transform: "rotate(-7deg)"
+      var image2 = new Image();
+      image2.src = cellData[i].icon;
+      image2.onload = onloadFunc;
+
+      fragment.append(image1);
+      fragment.append(image2);
     }
-  }).animate({
-    mode: "forwards",
-    duration: 2000,
-    easing: "linear",
-    time: "infinite",
-    direction: "alternate"
-  });
+    node.append(fragment);
 
-  c1Anim.run(c1);
-
-  // c2
-  var c2 = $("#circle2")[0];
-  var c2Anim = Amo.keyframes({
-    0: {
-      transform: "rotate(-5deg)"
-    },
-    20: {
-      transform: "rotate(13deg)"
-    },
-    60: {
-      transform: "rotate(2deg)"
-    },
-    80: {
-      transform: "rotate(11deg)"
-    },
-    100: {
-      transform: "rotate(4deg)"
-    }
-  }).animate({
-    mode: "forwards",
-    duration: 3000,
-    easing: "linear",
-    time: "infinite",
-    direction: "alternate"
-  });
-
-  c2Anim.run(c2);
+    // time out
+    setTimeout(() => {
+      onLoaded();
+    }, 6000);
+  })();
 });
